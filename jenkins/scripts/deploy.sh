2934a94bf4ab48fc925ed0f86f7d2e127354d770
NekoCoaster <3xtmegacoaster10@gmail.com>
2024-07-22 12:14:20 +0800

    Back again...

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v /home/nekocoaster/GitHub/jenkins-php-selenium-test/src:/var/www/html
+docker run -d -p 80:80 --name my-apache-php-app -v ~/GitHub/jenkins-php-selenium-test/src:/var/www/html
 sleep 1
 docker logs my-apache-php-app
 set +x
